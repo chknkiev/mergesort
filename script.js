f67556245f3c03cf757d43baa4826eed4d23f4ab
Bryan <suprnewb@gmail.com>
2022-08-16 17:22:28 +1000

    started function

---
+++ script.js
@@ -0,0 +1,21 @@
+// Split arr in half
+// Sort left arr
+// Sort right arr
+// Merge left and right into new arr
+
+
+
+
+function mergeSort(arr) {
+  if(arr.length < 2) {
+    return arr;
+  } else {
+
+  }
+}
+
+function merge(left, right) {
+  if((left.length > 0) && ( right.length > 0)) {
+    let sortedArr = []
+  }
+}
